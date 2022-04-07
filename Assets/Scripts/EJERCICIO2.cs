09e392ace1dee7534974cdb197ed4d67fa392d14
MARON MATIAS <46878355@ort>
2022-04-07 09:05:11 -0300

    Ejercicio 2

--- Assets/Scripts/EJERCICIO2.cs
+++ Assets/Scripts/EJERCICIO2.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class EJERCICIO2 : MonoBehaviour
 {
+
+    public int precio1;
+    public int precio2;
+    public int precio3;
+    public int dinerodisponible;
     // Start is called before the first frame update
     void Start()
     {
-        
+        int sumaprecios = precio1 + precio2 + precio3;
+
+        if(precio1+precio2+precio3 > dinerodisponible)
+        {
+            Debug.Log("La suma de los precios supera el dinero disponible.");
+            Debug.Log("Te falta" + (dinerodisponible - sumaprecios));
+        }
+        else
+        {
+            Debug.Log("Te alcanza");
+            Debug.Log("Te sobra " + (dinerodisponible - sumaprecios));
+        }
     }
 
     // Update is called once per frame

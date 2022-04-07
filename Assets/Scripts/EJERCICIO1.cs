a3015dbbc3407eebfaf329376e7494d5ae74ee5b
MARON MATIAS <46878355@ort>
2022-04-07 08:59:55 -0300

    Ejercicio 1 added

---
+++ Assets/Scripts/EJERCICIO1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJERCICIO1 : MonoBehaviour
+{
+    public int valor1;
+    public int valor2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(valor1 == valor2)
+        {
+            Debug.Log("Los valores son iguales");
+        }
+        else
+        {
+            Debug.Log("Los valores NO son iguales");
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
